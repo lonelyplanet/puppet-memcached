e56ec06b2b718ff4d74e1289dbd3cbd9abaf22f6
atlas2ninjas <atlas2ninjas@lonelyplanet.com.au>
2014-08-04 12:49:17 +1000

    Update to same version of memcached as AWS (1.4.14)

--- files/brews/memcached.rb
+++ files/brews/memcached.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Memcached < Formula
-  url "http://memcached.googlecode.com/files/memcached-1.4.13.tar.gz"
+  url "http://memcached.googlecode.com/files/memcached-1.4.14.tar.gz"
   homepage 'http://memcached.org/'
-  sha1 'd9a48d222de53a2603fbab6156d48d0e8936ee92'
-  version '1.4.13-boxen1'
+  sha1 'b360a6acf2454452c6fd4a5bdbbc303d85c3ec27'
+  version '1.4.14-boxen1'
 
   depends_on 'libevent'
 
